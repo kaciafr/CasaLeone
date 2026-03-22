005e2f65372573290a22422963604686e4b6e365
KaKoKa5 <bonfardeci.saverio@e-artsup.net>
2026-03-22 15:50:03 +0100

    AnguishBar

--- CasaLeone/Assets/Scripts/AngoisseBar/AngoisseBar.cs
+++ CasaLeone/Assets/Scripts/AngoisseBar/AngoisseBar.cs
@@ -5,14 +5,25 @@ public class AngoisseBar : MonoBehaviour
 {
 	[SerializeField] private GameObject anguishBar;
 	[SerializeField] private Image anguishBarImage;
+	[SerializeField] private Image DammageBarImage;
 
 	private void Start()
 	{
 		anguishBarImage.fillAmount = 0;
+		DammageBarImage.fillAmount = 0;
+	}
+
+	private void Update()
+	{
+		if (anguishBarImage.fillAmount <= DammageBarImage.fillAmount)
+		{
+			DammageBarImage.fillAmount = Mathf.Lerp(DammageBarImage.fillAmount,anguishBarImage.fillAmount, 0.05f);
+		}
 	}
 
 	public void AddAnguish(float add)
 	{
+		DammageBarImage.fillAmount += add;
 		anguishBarImage.fillAmount += add;
 	}
 
