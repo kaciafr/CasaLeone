ed44d8fc342cba01032f0eaa5372d2d78129275c
Vivien <viviane-rohaut@outlook.fr>
2026-03-24 17:50:26 +0100

    Debut du jeu cool
    Soleil qui bouge

--- CasaLeone/Assets/Scripts/ScriptMenu/MenuButtonAction.cs
+++ CasaLeone/Assets/Scripts/ScriptMenu/MenuButtonAction.cs
@@ -1,51 +1,90 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 public class MenuButtonAction : MonoBehaviour
 {
     public GameObject optionContainerGO;
-    private float speed = 5f;
-    bool isOptionOpen = false;
-    public string sceneName;
-    
-    public void LoadScene()
-    {
-        SceneManager.LoadScene(sceneName);
-    }
+    public GameObject restPanelGO;
+    public Light sunLight;
+
+    private float slideSpeed = 0.5f;
+    private float optionSpeed = 3f;
+    private float sunSpeed = 0.3f;
+    private bool isOptionOpen = false;
 
     private Vector3 closedPos;
     private Vector3 openPos;
 
+    private Quaternion lightDefaultRot;
+    private Quaternion lightTargetRot = Quaternion.Euler(20f, 0f, 0f);
+
     void Start()
     {
         closedPos = optionContainerGO.transform.localPosition;
-        openPos = closedPos + new Vector3(-300, 0, 0); 
+        openPos = closedPos + new Vector3(-300, 0, 0);
+        lightDefaultRot = sunLight.transform.rotation;
     }
 
     void Update()
     {
-        Vector3 target = isOptionOpen ? openPos : closedPos;
+        Vector3 targetPos = isOptionOpen ? openPos : closedPos;
         optionContainerGO.transform.localPosition = Vector3.Lerp(
             optionContainerGO.transform.localPosition,
-            target,
-            Time.deltaTime * speed
+            targetPos,
+            Time.deltaTime * optionSpeed
         );
-        
+
         if (Keyboard.current.escapeKey.wasPressedThisFrame)
-        {
             OpenOptions();
+    }
+
+    public void Begin()
+    {
+        if (restPanelGO != null)
+            StartCoroutine(SlidePanelDown());
+
+        StartCoroutine(RotateSun());
+    }
+
+    private IEnumerator SlidePanelDown()
+    {
+        Vector3 startPos = restPanelGO.transform.localPosition;
+        Vector3 endPos = startPos + new Vector3(0, -350, 0);
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * slideSpeed;
+            restPanelGO.transform.localPosition = Vector3.Lerp(startPos, endPos, t);
+            yield return null;
         }
+
+        restPanelGO.transform.localPosition = endPos;
+    }
+
+    private IEnumerator RotateSun()
+    {
+        Quaternion startRot = sunLight.transform.rotation;
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * sunSpeed;
+            sunLight.transform.rotation = Quaternion.Lerp(startRot, lightTargetRot, t);
+            yield return null;
+        }
+
+        sunLight.transform.rotation = lightTargetRot;
     }
 
     public void OpenOptions()
     {
         isOptionOpen = !isOptionOpen;
     }
-    
+
     public void Quit()
     {
         Application.Quit();
     }
-
 }
